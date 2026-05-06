82218199c5b6fe27c7d47cf69917419190cd8765
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-05-06 23:22:24 +0530

    building functions for start and type

--- main.py
+++ main.py
@@ -9,3 +9,33 @@ sentences = [
     'Artificial intelligence is changing the world',
     'coding imporves problem solving skills.'
 ]
+
+start_time = 0
+
+# start game
+def start_game():
+    global start_time
+
+    sentence = random.choice(sentences)
+    text_display.config(text=sentence)
+
+    entry.delete(0,tk.END)
+
+    result_label.config(text="")
+    
+    start_time = time.time()
+
+# check typing
+def check_typing():
+    end_time = time.time()
+
+    typed_text = entry.get()
+    original_text = text_display.cget('text')
+
+    time_taken = end_time - start_time
+
+    word = len(typed_text.split())
+
+    wpm = int((words/time_taken)*60)
+
+    correct_chars = 0
