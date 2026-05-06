2458cf393368a334ed6677d36ca724af9ba41302
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-05-06 23:10:10 +0530

    this is my python project on typing speed tes

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import random 
+import time
+
+# sample Sentences
+sentences = [
+    'Python is powerfull programming language.',
+    'Typing fast requires regular practice',
+    'Artificial intelligence is changing the world',
+    'coding imporves problem solving skills.'
+]
